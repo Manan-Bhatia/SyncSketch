7642a9d71d9ffcfaf1654e012c5299ef071b2fdf
Manan Bhatia <mananbhatia02@gmail.com>
2024-03-10 17:44:05 +0530

    created and integrated logout route

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
-import Image from "next/image";
-
+"use client";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 export default function Home() {
-    return <h1>Home</h1>;
+    const router = useRouter();
+    const logout = async () => {
+        try {
+            await axios.get("/api/logout");
+            router.push("/login");
+        } catch (error) {
+            console.log("Error in logout", error);
+        }
+    };
+    return (
+        <>
+            <h1>Home</h1>
+            <button onClick={logout}>Logout</button>
+        </>
+    );
 }
